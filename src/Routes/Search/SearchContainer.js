7d037b5558b2e16bb0d4f96aee99d35352e42dcb
jioun.lee.dev <jioun.lee@nts-corp.com>
2021-09-06 21:59:18 +0900

    #6 3. Search Container

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
+import { moviesApi, tvApi } from "api";
 
 export default class extends React.Component {
   state = {
@@ -9,6 +10,42 @@ export default class extends React.Component {
     loading: false,
     error: null,
   };
+
+  // check searchTerm is not on blank → execute function searchByTerm
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      this.searchByTerm();
+    }
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    this.setState({ loading: true });
+
+    try {
+      const {
+        data: { results: movieResults },
+      } = await moviesApi.search(searchTerm);
+      const {
+        data: { results: showResults },
+      } = await tvApi.search(searchTerm);
+
+      this.setState({
+        movieResults,
+        showResults,
+      });
+    } catch (e) {
+      this.setState({
+        error: "Can't find results.",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  };
+
   render() {
     const { movieResults, tvResults, searchTerm, loading, error } = this.state;
     return (
@@ -18,6 +55,7 @@ export default class extends React.Component {
         searchTerm={searchTerm}
         loading={loading}
         error={error}
+        handleSubmit={this.handleSubmit}
       />
     );
   }
